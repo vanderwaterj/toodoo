76603d41afa1226df3fdb1ffb5e6b496a521518f
Jonathan <jonathan.vanderwater@gmail.com>
2019-06-15 03:45:09 -0500

    More GUI

--- gui/scripts/menuHandler.js
+++ gui/scripts/menuHandler.js
@@ -2,6 +2,30 @@ const { remote } = require('electron');
 
 const win = remote.getCurrentWindow();
 
+const iconSize = 14;
+
+$('body').prepend(`<header class="header">
+<h1 class="unselectable logo">
+    <span class="helper"></span><a href="index.html" draggable="false"><img src="../media/toodooText.png"
+    alt="toodoo icon" height="20" width="80" draggable="false"></a>
+</h1>
+
+<ul class="main-nav">
+    <li class="frame" id="settings">
+        <span class="helper"></span><a href="#"><img src="../media/settings.svg" height="${iconSize}" width="${iconSize}"></a>
+    </li>
+    <li class="frame" id="minimize">
+        <span class="helper"></span><a href="#"><img src="../media/minimize.svg" height="${iconSize}" width="${iconSize}"></a>
+    </li>
+    <li class="frame" id="maximize">
+        <span class="helper"></span><a href="#"><img src="../media/fullscreen.svg" height="${iconSize}" width="${iconSize}"></a>
+    </li>
+    <li class="frame" id="close">
+        <span class="helper"></span><a href="#"><img src="../media/close.svg" height="${iconSize}" width="${iconSize}"></a>
+    </li>
+</ul>
+</header>`);
+
 $('#minimize').click(() => {
     win.minimize();
 });
